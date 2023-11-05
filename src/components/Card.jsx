08920224bdfa625b881b9691afc104ff2d548b12
devibifaa <ib2link@yahoo.com>
2023-11-05 03:14:49 +0100

    first card design using tailwind

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+// import {background-img} from '../public/background-img'
+
+export const Card= ()=> {
+  return (
+    <div className='card bg-gray-200  w-[100%]  h-[100vh]  rounded-3xl flex justify-center items-center'>
+        <div className='card max-w-lg justify-center items-center p-10 rounded-lg'>
+       <div className='gap-2 flex '>
+        <div className="logo h-10 w-10 bg-cyan-600 items-center flex justify-center rounded-full text-[#fff]">
+                ib
+        </div>
+
+        <h4 className='text-[30px] text-[#2f5044]'>Work<span className='text-cyan-600'>station</span></h4>
+
+        </div>
+        <div className="card-mg  items-center justify-center" >
+        <img className=' flex justify-center items-center h-[250px] w-[80%] mt-5 rounded-xl shadow-xl' src="public/background-img 1.jpg" alt="image of computer " />
+        <h1 className='text-xl mt-5 font-medium'>Freelance Frontend Developer</h1>
+        <h2 className='text-cyan-600'>Working from the comfort of home </h2>
+        <p className='w-[80%] mt-2'>Passionate Frontend Developer that is creating beautiful and responsive Website using React, JavaScript, CSS, tailwind and HTML</p>
+        <button className='bg-cyan-600 p-2 rounded-[10px] w-[120px]  mt-5 text-white text-lg'>Hire me</button>
+        </div>
+
+      
+
+
+    </div>
+    </div>
+  )
+}
