3d62d4827e8ee73ad11f493ab56d38c8f8278cb0
devibifaa <ib2link@yahoo.com>
2023-11-05 04:25:35 +0100

    deployment update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,8 +17,12 @@ export const Card= ()=> {
         <img className=' flex justify-center items-center h-[250px] w-[80%] mt-5 rounded-xl shadow-xl' src="public/background-img 1.jpg" alt="image of computer " />
         <h1 className='text-xl mt-5 font-medium'>Freelance Frontend Developer</h1>
         <h2 className='text-cyan-600'>Working from the comfort of home </h2>
-        <p className='w-[80%] mt-2'>Passionate Frontend Developer that is creating beautiful and responsive Website using React, JavaScript, CSS, tailwind and HTML</p>
-        <button className='bg-cyan-600 p-2 rounded-[10px] w-[120px]  mt-5 text-white text-lg'>Hire me</button>
+        
+        <p className='w-[80%] mt-2 text-gray-600'>Passionate Frontend Developer that is creating beautiful and responsive Website using React, JavaScript, CSS, tailwind and HTML</p>
+       
+       <div>
+       <a className='bg-cyan-600 p-2 inline-block  uppercase rounded-[5px] w-[120px]  shadow-lg text-center mt-5 text-white text-lg'>Hire me</a>
+        </div>
         </div>
 
       
